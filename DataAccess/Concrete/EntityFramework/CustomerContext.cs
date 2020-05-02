9faa8c1c088d099ac08800fec06597d380f57b45
ereninal10@gmail.com <ereninal10@gmail.com>
2020-05-02 22:39:53 +0300

    Business layer context updated.

---
+++ DataAccess/Concrete/EntityFramework/CustomerContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class CustomerContext : DbContext
+    {
+        //Buradaki nesneler ile veritabanındaki tablo nesnelerini eşleştiricez
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=AtmDB;Trusted_Connection=true");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Customer : IEntity
+    {
+        public int Id { get; set; }
+        public string CustomerNo { get; set; }
+        public string CardNumber { get; set; }
+        public string AccountNumber { get; set; }
+        public string IdentificationNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CardPassword { get; set; }
+        public string AccountPassword { get; set; }
+    }
+}
